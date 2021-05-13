560c1a6ca49f6f90be182ffc66d77cfaea6b0c1a
ysz951 <shengyang_zhou@hotmail.com>
2021-05-12 23:22:15 -0500

    init

---
+++ src/main/java/com/dont/forget/DontForgetServerApplication.java
@@ -0,0 +1,13 @@
+package com.dont.forget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontForgetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontForgetServerApplication.class, args);
+	}
+
+}
