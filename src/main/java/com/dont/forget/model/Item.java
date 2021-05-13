e478504bcab6707174a2163493a1b1b65d833f8b
ysz951 <shengyang_zhou@hotmail.com>
2021-05-13 01:16:14 -0500

    Add item model

--- src/main/java/com/dont/forget/model/Item.java
+++ src/main/java/com/dont/forget/model/Item.java
@@ -1,13 +1,11 @@
 package com.dont.forget.model;
 
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
@@ -17,32 +15,23 @@ import com.sun.istack.NotNull;
 
 @Entity
 @Table
-public class Users {
+public class Item {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
-    @Size(min = 1, max = 25)
-    @Column(name = "user_name")
-    private String userName;
+    @Size(min = 1, max = 40)
+    @Column(name = "item_name")
+    private String itemName;
 
-    @NotNull
-    @Size(min = 6)
-    private String password;
-
-
-    @OneToMany(
-            mappedBy = "user",
-            targetEntity = Item.class,
-            fetch = FetchType.EAGER
-            )
+    @ManyToOne(targetEntity = Users.class)
     @JsonIdentityInfo(
             generator = ObjectIdGenerators.PropertyGenerator.class,
             property = "id")
     @JsonIdentityReference(alwaysAsId=true)
-    private List<Item> userItem;
+    private Users user;
 
     public Long getId() {
         return id;
@@ -52,28 +41,20 @@ public class Users {
         this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getItemName() {
+        return itemName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
     }
 
-    public String getPassword() {
-        return password;
+    public Users getUser() {
+        return user;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<Item> getUserItem() {
-        return userItem;
-    }
-
-    public void setUserItem(List<Item> userItem) {
-        this.userItem = userItem;
+    public void setUser(Users user) {
+        this.user = user;
     }
 
 
